41b1c055e5e43d1171c35da73bbe3b9df1934de7
RicentCB <ricardo.cb009@gmail.com>
2019-09-14 15:20:48 -0500

    Metodo crear autoamata dada una experion regular y asignarle un token. Metodo crear una union de auomatas dado un arreglo de tokens y de expersiones regulares

--- AFD.py
+++ AFD.py
@@ -2,6 +2,7 @@ import sys
 
 from alphabet import Alphabet
 from regEx import RegularExp
+from AFN import AFN
 
 class AFD():
     id_AFD = 0
@@ -10,11 +11,11 @@ class AFD():
     #end_state (Arreglo de enteros)
     #Alphabet (Arrelglo de: caracteres, simbolos especiales, palabras etc)
     #Token = ?
-    def __init__(self, ini_state, end_state, transitions, alphabet, token = None):
+    def __init__(self, ini_state, end_states, transitions, alphabet, token = None):
         self.id_AFD = AFD.id_AFD
         AFD.id_AFD = AFD.id_AFD +1
         self.ini_state = ini_state
-        self.end_state = end_state
+        self.end_states = end_states
         self.transitions = transitions
         self.alphabet = alphabet
         self.token = token
@@ -30,7 +31,8 @@ class AFD():
         return -1
 
     #Funcion que imprime la tabla de transicion del AFD
-    def printTrasitionTable(self):
+    def printTransitionTable(self):
+
         matrixFinal = self.transitions     #Conseguimos la matriz de transciones
         allSymbols = []
         statesFrom = []
@@ -41,26 +43,27 @@ class AFD():
             if column[0] not in statesFrom:
                 statesFrom.append(column[0])    #Traes Estados Inciales
         #Recorrer Arreglo de simbolos
-        print("\n  Estado   ",end="")
+        print("\n  Estado | ",end="")
         for elem in allSymbols:
             out = ""
             if elem == Alphabet.range_num:
                 out = "[0-9]"  
             elif elem == Alphabet.range_min:
-                out = "[A-Z]"  
+                out = "[a-z]"  
             elif elem == Alphabet.range_may:
-                out = "[a-z]"
+                out = "[A-Z]"
             else:
                 out = elem
-            print("| ",out," |", end="")
+            print("  {:>2}  |".format(out), end="")
         print("| token |", end="")
         print("\n")
         
         cont = 0
         for state in statesFrom:
-            print("  ",state,"  | ", end="")
+            print("    {:>2}   | ".format(state), end="")
             for symbol in allSymbols:
-                print("  ",self.funcion_transicion(matrixFinal,state,symbol), "  | ",end="")
+                car = self.funcion_transicion(matrixFinal,state,symbol)
+                print('   {:>2}  |'.format(car),end="")
             for tupla in matrixFinal:
                 if tupla[0] == state:
                     print(" || ",tupla[3])
@@ -70,8 +73,57 @@ class AFD():
 
     #Funcion que genera un AFD apartir una de una expresion regular 
     # ademas de agregar un token dado, en su estado de aceptacion
-
-    def createAFDexpRegular(self, string, token):
+    #Regresa un objeto AFD
+    def createAFDexpRegular(string, token=None):
         EXPReg = RegularExp(string)
         AFNReg = EXPReg.createAFN()
+        print(AFNReg)
+        AFNReg.end_state.token = token  #Asigna el token al estado de aceptacion del AFN
+        arrayAFD =  AFNReg.convert_to_afd() #Devuleve un arreglo de 3 parametros para crear un AFD 
+        AFDReg = AFD(0, arrayAFD[0], arrayAFD[1], arrayAFD[2])
+        return AFDReg
 
+    #Funcion que une arreglo de expresiones regulares dando a cada un token diferebte
+    # Recibe arreglo de expresiones regulares, arreglo de tokenes (misma longitud)
+    # Regresa un AFD
+    def union_nAFDs(arrayRegExps, arrayTokens, arrayLetters):
+        if isinstance(arrayRegExps,list) and isinstance(arrayTokens, list):
+            if len(arrayRegExps) == len(arrayTokens):
+                arrayAFNS = []
+                #Se crean los AFNS a partir de la expreison Regular
+                for i in range(0,len(arrayTokens)):
+                    #Creamos el AFN con la expresion regular
+                    RegExpAux = RegularExp(arrayRegExps[i])
+                    AFNRegExpAux = RegExpAux.createAFN()
+                    #Lo insertamos en la lista de AFN's
+                    arrayAFNS.append(AFNRegExpAux)
+                # arrayAFNS.reverse()
+                #Llamamos al metodo de union de AFN's
+                mainAFN = AFN.union_nAFN(arrayAFNS, arrayTokens)
+                #Convertimos a AFD
+                arrayAFD = mainAFN.convert_to_afd(arrayLetters)
+                #Creamos el Objeto AFD
+                AFDReturn = AFD(0,arrayAFD[0], arrayAFD[1], arrayAFD[2])
+                return AFDReturn
+        else:
+            print("Error")
+            sys.exit() 
+#P = '+'
+#M = '-'
+#N = '[0-9]'
+#D = '.'
+#A = '[A-z]'
+#a = '[a-z]'
+print("---- A   F   D ----")
+RegExp1 = "(P|M)?&(N+)&D&(N+)"  #(+|-)?&[0-9]+&.&[0-9]+
+RegExp2 = "(P|M)?&(N+)"         #(+!-)?&[0-9]?
+RegExp3 = "(a|A)&(a|A|N)*"      #([a-z]|[A-Z])&([a-z]|[A-Z]|[0-9])*
+RegExp4 = "P&P"                 #+&+
+RegExp5 = "P"                   #+
+
+arrayRegExp = [RegExp1, RegExp2, RegExp3, RegExp4, RegExp5]
+arrayToken = [10,20,30,40,50]
+alphabet = ['P', 'M', 'N', 'D', 'a', 'A']
+
+mainAFD = AFD.union_nAFDs(arrayRegExp, arrayToken, alphabet)
+mainAFD.printTransitionTable()
